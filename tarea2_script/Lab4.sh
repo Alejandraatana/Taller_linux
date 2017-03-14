916322e4dd247c919f75766e88757f34b31b2ba8
Alejandra <alejandra.atana.rodriguez@gmail.com>
2017-03-13 23:13:58 -0600

    tarea terminada

--- tarea2_script/Lab4.sh
+++ tarea2_script/Lab4.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
 echo Dame dos cadenas
- cad1="" 
- cad2=""
+	read cad1
+        read cad2	
+ funcion1(){
+	if [ -z "$cad1" ] ;then
+		echo La longitud de la cadena 1 es 0
+	fi
+	if [ -n "$cad2" ] ;then
+		echo La longitud de la cadena 2 no es 0
+	fi
+}
+funcion2(){
+	echo La longitud de la cadena 1 es: ${#cad1}
+	echo La longitud de la cadena 2 es: ${#cad2}
+	if [ ${#cad1} -eq ${#cad2} ] ;then
+		echo Tienen la misma longitud
+	fi
+}
+funcion3(){
+	if [ "$cad1" = "$cad2" ] ;then
+		echo Son iguales
+	fi
+}
 
-if [ -z $cad1 ] ;then
-	echo La longitud de la cadena 1 es 0
-fi
-
-if [ -n $cad2 ] ;then
-	echo La longitud de la cadena 2 no es 0
-fi
+funcion1
+funcion2
+funcion3
